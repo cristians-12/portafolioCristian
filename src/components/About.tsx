187caa68420de8f2994a6d11e22e1ff63015949d
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-24 23:14:22 -0500

    about nov

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,11 @@ function About() {
   return (
     <>
       <section className="lg:px-[15rem] px-[3rem]">
-        <h3 className="text-xl font-medium my-5">Introduccion</h3>
+        <h3 className="text-xl font-bold my-4">Introduccion</h3>
         <p className="text-2xl text-gray-400">
-          Soy ingeniero electronico, desarrollador de aplicativos moviles y web
-          con 2 años de experiencia en el area.
+          Me llamo <span className="text-red-600 font-bold">Cristian</span>, soy
+          ingeniero electronico, apasionado por la tecnologia y desarrollador de
+          aplicativos moviles y web con 2 años de experiencia en el area.
         </p>
       </section>
     </>
