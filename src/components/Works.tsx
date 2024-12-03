a8fb22664fb35584425da352e519c1d6d102901c
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-12-03 11:30:09 -0500

    proyectos

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,7 +4,9 @@ import Card from "./cards/Card";
 function Works() {
   return (
     <section className="my-[50px]">
-      <h3 className="text-3xl font-bold text-red-600 my-3">Proyectos:</h3>
+      <h3 className="text-3xl font-bold text-red-600 my-3">
+        Algunos de mis proyectos:
+      </h3>
       <article className="flex gap-10 flex-col lg:flex-row">
         {works.map((work) => (
           <Card
