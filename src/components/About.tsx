ec48e46b33a74519d0bdbd73d761c81fba544513
cristian.sanchez.casd <cristian.sanchez.casd@gmail.com>
2026-01-21 22:24:41 +0000

    feat/ text styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,31 +7,32 @@ function About() {
         <h3 className="text-xl font-bold my-4 text-red-600">Introduccion</h3>
         <p className="text-2xl text-gray-400">
           Me llamo <span className="text-red-600 font-bold">Cristian</span>, soy
-          <span className="font-semibold"> ingeniero electrónico</span>,
-          apasionado por la
+          <span className="font-semibold"> ingeniero electrónico</span> apasionado por la
           <span className="font-semibold"> tecnología</span> y especializado en
           el desarrollo de
           <span className="font-semibold"> aplicaciones móviles</span> y
           <span className="font-semibold"> web</span>. Cuento con
           <span className="font-semibold"> 4 años de experiencia</span> en el
-          área. Me enfoco en desarrollar código limpio, escalable y legible utilizando principios SOLID y
+          área de la tecnología y me enfoco en desarrollar código limpio, escalable y mantenible utilizando principios SOLID y
           aplicando siempre las mejores prácticas de programación para
-          garantizar calidad y mantenibilidad.
+          garantizar calidad.
         </p>
+        <br />
         <p className="text-2xl text-gray-400">
           Tengo proyectos y experiencia con{" "}
           <span className="font-semibold">MySQL</span>,{" "}
           <span className="font-semibold">PostgreSQL</span>,{" "}
+          <span className="font-semibold">React Native</span>,{" "}
+          <span className="font-semibold">Kotlin</span>,{" "}
           <span className="font-semibold">MongoDB</span> y{" "}
           <span className="font-semibold">Firebase</span>. También he trabajado
           con <span className="font-semibold">Node.js</span>,{" "}
           <span className="font-semibold">Express</span>,{" "}
           <span className="font-semibold">React</span>, y{" "}
-          <span className="font-semibold">Next.js</span>, desarrollando
-          aplicaciones escalables y de alto rendimiento. Además, tengo
-          experiencia en la implementación de{" "}
+          <span className="font-semibold">Next.js</span> desarrollando
+          aplicaciones escalables y de alto rendimiento. Cuento tambien con experiencia en la implementación de{" "}
           <span className="font-semibold">APIs REST</span> y{" "}
-          <span className="font-semibold">GraphQL</span>, garantizando una
+          <span className="font-semibold">GraphQL</span> garantizando una
           comunicación eficiente entre cliente y servidor.
         </p>
       </section>
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,7 +4,7 @@ import Card from "./cards/Card";
 function Works() {
   return (
     <section className="my-[50px]">
-      <h3 className="text-3xl font-bold text-red-600 my-3">
+      <h3 className="text-3xl font-bold text-red-600 my-10">
         Algunos de mis proyectos:
       </h3>
       <article className="flex gap-10 flex-col justify-center lg:flex-wrap lg:flex-row">
